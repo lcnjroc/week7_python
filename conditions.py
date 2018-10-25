b27d3a8b6d5bd59e33d1eed9fb6ee3236ff5096b
Jared M <lcnjroc@gmail.com>
2018-10-25 14:26:31 -0400

    more updates

--- conditions.py
+++ conditions.py
@@ -1,1 +1,20 @@
-print("This is the conditions file")
+print("Rules that govern the state of water")
+
+current_temp= false
+
+while current_temp is false
+	current_temp= input("Enter a temperature \n")
+	print(current_temp)
+
+	if(int(current_temp) < 0) or (int(current_temp== 0)):
+		print("water is a solid. icy!")
+		current_temp= false
+
+	elif(int(current_temp) < 100):
+		print("water is a liquid. profit!")
+		current_temp= false
+
+	elif(int(current_temp) > 100) or (int(current_temp== 100)):
+		print("water is a vapor. drop it like it's hot!")
+		current_temp= false
+		

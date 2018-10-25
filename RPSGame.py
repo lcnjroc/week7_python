908e78098399464b0f5997ead939224a0746a794
Jared M <lcnjroc@gmail.com>
2018-10-25 13:58:45 -0400

    newness and updates

--- RPSGame.py
+++ RPSGame.py
@@ -2,6 +2,7 @@ from random import randint
 
 # available weapons => store this in an array
 choices = ["Rock", "Paper", "Scissors"]
+player = false
 
 # make the computer pick one at random
 computer_choice = choices[randint(0, 2)]

523cd7b7de5363774fbc92a62e6678e27dc75b02
Jared M <lcnjroc@gmail.com>
2018-11-01 13:29:16 -0400

    add functions and basic function

---
+++ functions.py
@@ -0,0 +1,9 @@
+# lets explore some functions and write them out
+# and also what they do
+
+def greeting():
+	# say hello
+	print("hello from your first function")
+
+# this is how you call or invoke a function
+greeting()
